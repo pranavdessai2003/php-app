98083eb89528c07c1e7a94bd624b3c42398a75ab
Pranav Dessai <73122705+pranavdessai2003@users.noreply.github.com>
2024-05-13 20:22:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 </head>
 <body>
     <h1>Hello, World!</h1>
-    <p>This is a simple PHP application.</p>
+    <p>This is a simple PHP application deployed on Red Hat's OpenShift Platform.</p>
 </body>
 </html>
